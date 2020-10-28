72b2b05d8174dc181bf13ff5dd263992de36b192
Nurbek03 <nurbekabdilamit@gmail.com>
2020-10-28 13:26:27 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myFavoritefood = "";
+        int nam = 2;
+        String word = "Food";
+        Scanner s = new Scanner(System.in);
+
+        System.out.println(nam + word);
+
+        System.out.println(myFavoritefood = word);
+
+        System.out.println("Вы сохранили положительное число");
+
+        {
+
+            System.out.println("Введите ваша имя");
+            String string = s.nextLine();
+            System.out.println("привет " + string);
+          }
+        }
+     }
